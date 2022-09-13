74ccc1f96238136947939d3140a142a4e01c803e
Ben Siebrase <ben.siebrase@du.edu>
2022-09-12 18:17:18 -0600

    added modifier.py

---
+++ modifier.py
@@ -0,0 +1,19 @@
+def modify(x):
+    """takes a string as input and joins chars with **"""
+
+    modified_x=''
+
+    for i,j in enumerate(x):
+        if i < len(x) -1:
+            modified_x += j + "**"
+        else:
+            modified_x += j
+    
+    return modified_x
+
+def main():
+    to_mod = input('Provide a string:')
+    print(modify(to_mod))
+
+if __name__ == "__main__":
+    main()
